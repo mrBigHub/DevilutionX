cd1de9043623e25289c4823b19350db4a3ab3725
mrBigHub <mr.biks@yahoo.com>
2026-09-22 21:22:44 +0700

    Update place monster.cpp

--- Source/monster.cpp
+++ Source/monster.cpp
@@ -3742,7 +3742,9 @@ std::expected<void, std::string> InitMonsters()
 		if (currlevel == 16) {
 			for (size_t i = 0; i < LevelMonsterTypeCount; i++) {
 				if (LevelMonsterTypes[i].type == MT_STORMCLAW) {
-					for (int n = 0; n < 20; n++);
+					for (int n = 0; n < 20; n++){
+						PlaceMonster(i);
+					}
 					break;
 				}
 			}	
